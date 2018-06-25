80460b044374b6ab4e27607d22d7e155dd7e0eff
Mohamed Khaled <themidozedan@gmail.com>
2018-06-25 10:02:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,9 +11,9 @@ include('head.php')
   <div class="container">
     <div class="row">
       <div class="col-sm-6">
-      <h1>The ease & simplicity of Gmail, available across devices</h1>
+      <h1>The ease & simplicity of MaiLock, available across devices</h1>
       <button class="btn btn-danger" > 
-     Creat Your Free Accont Now  
+     Create Your Free Accont Now  
       </button>
       </div>
 
