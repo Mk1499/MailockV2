38e414a4171dd307528454aef328516e43c99352
Mohamed Khaled <themidozedan@gmail.com>
2018-06-26 15:17:47 +0200

    Add files via upload

---
+++ Signup.php
@@ -0,0 +1,43 @@
+<?php
+include('head.php');
+?>
+
+<!-- Write Code Here --> 
+<link href="CSS/signin.css" rel="stylesheet" > 
+
+<div class="login-area">
+        <div class="bg-image">
+            <div class="login-signup">
+                <div class="container">
+
+<div id="register" class="tab-pane">
+        <div class="login-inner">
+             <div class="title">
+                 <h1>Welcome <span>back!</span></h1>
+             </div>
+             <div class="login-form">
+                 <form>
+                     <div class="form-details">
+                         <label class="user">
+                             <input type="text" name="username" placeholder="Full Name" id="username">
+                         </label>
+                         <label class="mail">
+                             <input type="email" name="mail" placeholder="Email Address" id="mail">
+                         </label>
+                         <label class="pass">
+                             <input type="password" name="passsword" placeholder="Password" id="password">
+                         </label>
+                         <label class="pass">
+                             <input type="password" name="passsword" placeholder="Confirm Password" id="password">
+                         </label>
+                     </div>
+                     <button type="submit" class="form-btn" onsubmit="">Sent</button>
+                 </form>
+             </div>
+         </div>
+     </div>
+     </div>></div></div></div>
+
+     <?php 
+include('foot.php') ; 
+?>
--- signin.php
+++ signin.php
@@ -5,32 +5,37 @@ include('head.php');
 <!-- Write Code Here --> 
 <link href="CSS/signin.css" rel="stylesheet" > 
 
-<section class="signIn">
-    <div class="shadow">
-<h1 class="text-center"> Sign In </h1>
+ <div class="login-area">
+        <div class="bg-image">
+            <div class="login-signup">
+                <div class="container">
+                        <div id="login" >
+                            <div class="login-inner">
+                                <div class="title">
+                                    <h1>Welcome <span>back!</span></h1>
+                                </div>
+                                <div class="login-form">
+                                    <form>
+                                        <div class="form-details">
+                                            <label class="user">
+                                                <input type="text" name="username" placeholder="Username" id="username">
+                                            </label>
+                                            <label class="pass">
+                                                <input type="password" name="passsword" placeholder="Password" id="password">
+                                            </label>
+                                        </div>
+                                        <button type="submit" class="form-btn" onsubmit="">Sent</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    
 
-<div class="container">
-    <div class="row">
-        <div > 
-<form>
-  <div class="form-group">
-    <label>Email address</label>
-    <input type="email" class="form-control">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label>Password</label>
-    <input type="password" class="form-control">
-  </div>
- 
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-</div>
-</div>
-</div>
-</div>
-</div>
-</section>
 <?php 
 include('foot.php') ; 
 ?>
